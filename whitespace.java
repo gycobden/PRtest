796e29052f4e3f1634c45eeb2d3a6a8fca5d53e5
Graham Cobden <gycobden@gmail.com>
2026-01-05 12:52:29 -0800

    added a new java file

---
+++ whitespace.java
@@ -0,0 +1,11 @@
+public class whitespace {
+  public static void main(String[] args) {
+whitespace(); 
+  }
+
+    public static void whitespace() {
+        System.out.println("I");
+System.out.println("love");
+          System.out.println("using whitespace!");
+}
+}
