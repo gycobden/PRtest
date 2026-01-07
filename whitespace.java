eddddf32c7412baec0fe50bf9602f8c2d6c8eabc
Graham <147366490+gycobden@users.noreply.github.com>
2026-01-06 16:31:54 -0800

    added file describing love for whitespace

---
+++ whitespace.java
@@ -0,0 +1,12 @@
+public class whitespace {
+  public static void main(String[] args) {
+    printWhitespaceLove(); 
+  }
+
+  // prints sentence about love for whitespace
+  public static void printWhitespaceLove() {
+    System.out.println("I");
+    System.out.println("love");
+    System.out.println("using whitespace!");
+  }
+}
